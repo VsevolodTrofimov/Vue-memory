916d37fa190bfa9e8f2468ecd38257b7defa9f16
v-trof <seva.trofimov2@gmail.com>
2018-02-16 22:29:48 +0300

    Added routing mutations tests

---
+++ spec/store/mutations/routing.spec.js
@@ -0,0 +1,20 @@
+import * as mutations from '@/src/store/mutations.js'
+
+describe('Mutations', () => {
+  const state = {}
+  beforeEach(() => {
+    Object.assign(state, {
+      stage: 'Start'
+    })
+  })
+
+  describe('setStage', () => {
+    it('Changes stage', () => {
+      mutations.setStage(state, 'End')
+      expect(state.stage).toBe('End')
+
+      mutations.setStage(state, 'Game')
+      expect(state.stage).toBe('Game')
+    })
+  })
+})
